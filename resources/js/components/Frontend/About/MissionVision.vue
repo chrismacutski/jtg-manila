ff7488091618aa09b70c3c5b98e085fa90ce03ae
Chris Macuto <christopher.macuto@teleserv.com.ph>
2019-06-22 20:15:44 +0800

    adding the mission and vision text

--- resources/js/components/Frontend/About/MissionVision.vue
+++ resources/js/components/Frontend/About/MissionVision.vue
@@ -4,8 +4,8 @@
         <div class="container">
             <div :style="{'height' : fullheight}" class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
                 <div class="col-md-10 text-center jtg-animate" data-scrollax=" properties: { translateY: '70%' }">
-                    <!--<h1 class="mb-0">ABOUT US</h1>-->
-                    <!--<h3 class="subheading mb-4 pb-1">Christ is the Vision Making Disciples is our Mission</h3>-->
+                    <h1 class="mb-0">MISSION AND VISION</h1>
+                    <h3 class="subheading mb-4 pb-1">Christ is the Vision Making Disciples is our Mission</h3>
 
                     <div class="mouse2">
                         <a href="#" class="mouse-icon">
