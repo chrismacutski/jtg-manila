ca53837a8762dbf35962ed73e387c0e40a6fcd43
Chris Macuto <christopher.macuto@teleserv.com.ph>
2019-06-22 09:17:40 +0800

    modifying service times

--- resources/views/frontend/partials/home/footer.blade.php
+++ resources/views/frontend/partials/home/footer.blade.php
@@ -4,7 +4,7 @@
             <div class="col-md">
                 <div class="jtg-footer-widget mb-4">
                     <h2 class="jtg-heading-2">JTG Manila Church</h2>
-                    <p>Christ is our Vision! Making Disciples is our Mission!</p>
+                    <p class="text-white">Christ is our Vision! Making Disciples is our Mission!</p>
                     <ul class="jtg-footer-social list-unstyled mt-5">
                         {{--<li class="jtg-animate"><a href="#"><span class="icon-twitter"></span></a></li>--}}
                         <li class="jtg-animate"><a href="https://www.facebook.com/jesusthegospelmanilachurch/" target="_blank"><span class="icon-facebook"></span></a></li>
@@ -16,16 +16,21 @@
                 <div class="jtg-footer-widget mb-4">
                     <h2 class="jtg-heading-2">About</h2>
                     <ul class="list-unstyled">
-                        <li><a href="{{ route('frontend.about.vision') }}" class="py-1 d-block"></span><span class="ion-ios-arrow-forward mr-3"></span>Vision</a></li>
+                        <li><a href="{{ route('frontend.about.history') }}" class="py-1 d-block"></span><span class="ion-ios-arrow-forward mr-3"></span>Our Story</a></li>
+                        <li><a href="{{ route('frontend.about.vision') }}" class="py-1 d-block"></span><span class="ion-ios-arrow-forward mr-3"></span>Mission and Vision</a></li>
+                        <li><a href="{{ route('frontend.about.senior-pastors') }}" class="py-1 d-block"></span><span class="ion-ios-arrow-forward mr-3"></span>Senior Pastors</a></li>
                         <li><a href="{{ route('frontend.about.leadership') }}" class="py-1 d-block"></span><span class="ion-ios-arrow-forward mr-3"></span>Leadership</a></li>
-                        <li><a href="{{ route('frontend.about.history') }}" class="py-1 d-block"></span><span class="ion-ios-arrow-forward mr-3"></span>History</a></li>
+                        <li><a href="{{ route('frontend.about.departments') }}" class="py-1 d-block"></span><span class="ion-ios-arrow-forward mr-3"></span>Department</a></li>
+                        <li><a href="{{ route('frontend.about.ministries') }}" class="py-1 d-block"></span><span class="ion-ios-arrow-forward mr-3"></span>Ministry</a></li>
+                        <li><a href="{{ route('frontend.about.real-made-teachings') }}" class="py-1 d-block"></span><span class="ion-ios-arrow-forward mr-3"></span>REAL - MADE System</a></li>
+                        <li><a href="{{ route('frontend.about.what-we-believe') }}" class="py-1 d-block"></span><span class="ion-ios-arrow-forward mr-3"></span>What we believe</a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-md">
                 <div class="jtg-footer-widget mb-4">
                     <h2 class="jtg-heading-2">Location</h2>
-                    <p><span>1001 Lope De Vega St. Corner Rizal Avenue, Sta. Cruz Manila, Philippines</span></p>
+                    <p class="text-white"><span>1001 Lope De Vega St. Corner Rizal Avenue, Sta. Cruz Manila, Philippines</span></p>
                     <a class="btn btn-white px-4 py-2" target="_blank" href="https://www.google.com/maps/place/JTG+MANILA+CHURCH/@14.6065826,120.9818983,19.99z/data=!4m5!3m4!1s0x3397ca013563de8b:0xae0ac353f39b4d5d!8m2!3d14.6066601!4d120.9820092">GET DIRECTIONS</a>
                 </div>
             </div>
@@ -34,9 +39,10 @@
                     <h2 class="jtg-heading-2">Service Hours</h2>
                     <div class="opening-hours">
                         <p>
-                            <span class="mb-3">Sunday Morning Service <br> 10:00 AM to 01:00 PM</span>
-                            <span class="mb-3">Sunday Afternoon Service <br> 4:00 PM to 6:00 PM</span>
-                            <span>Midweek Service (Thursday) <br> 7:00 PM to 9:00 PM</span>
+                            <span class="mb-3">Sunday Morning Celebration <br> 10 AM</span>
+                            <span class="mb-3">Sunday Afternoon Celebration <br> 4 PM</span>
+                            <span class="mb-3">Thursday Midweek Service <br> 8 PM</span>
+                            <span class="mb-3">Haven (Youth Service) <br> 3 PM</span>
                         </p>
                     </div>
                 </div>
